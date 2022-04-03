3a79df8a1749c7eb509be6f442f788844a85933d
Aleksa012 <pesicaleksa042@gmail.com>
2022-04-04 01:27:41 +0200

    fixing card details page

--- pages/[card-details].js
+++ pages/[card-details].js
@@ -1,27 +1,55 @@
-// import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 
-// import { useRouter } from "next/router";
-// import Card from "../components/Card";
+import { useRouter } from "next/router";
 
-// function CardDetails() {
-//   const router = useRouter();
-//   const [card, setCard] = useState({});
-//   const cardId = router.query["card-details"]
-//     ? router.query["card-details"]
-//     : "80143954";
+import Image from "next/image";
 
-//   useEffect(() => {
-//     async function getCard() {
-//       const data = await fetch(
-//         `https://db.ygoprodeck.com/api/v7/cardinfo.php?id=${cardId}`
-//       ).then((res) => res.json());
+function CardDetails() {
+  const router = useRouter();
+  const [card, setCard] = useState(null);
+  const cardId = router.query["card-details"];
 
-//       setCard(data.data[0]);
-//     }
-//     getCard();
-//   }, [cardId]);
+  useEffect(() => {
+    async function getCard() {
+      const data = await fetch(
+        `https://db.ygoprodeck.com/api/v7/cardinfo.php?id=${cardId}`
+      ).then((res) => res.json());
 
-//   return <div>{card && <Card img={card.card_images[0].image_url} />}</div>;
-// }
+      setCard(data.data[0]);
+    }
+    getCard();
+  }, [cardId]);
 
-// export default CardDetails;
+  return (
+    <Fragment>
+      {card && (
+        <Image width={500} height={680} src={card.card_images[0].image_url} />
+      )}
+      {card && (
+        <div className="card-details">
+          <h2>{card.name}</h2>
+          <p>
+            Level: {card.level} <br />
+            Type: {card.type} <br />
+            Race: {card.race} <br />
+            Attribute: {card.attribute} <br />
+            Description: {card.desc} <br />
+            Price: $
+            {card.card_prices
+              ? card.card_prices[0].ebay_price
+              : "Not in stores!"}
+            on E-bay <br />
+            Sets:{" "}
+            {card.card_sets
+              ? card.card_sets.map((set) => set.set_name + " ").slice(0, 5)
+              : "No sets!"}
+            <br />
+            and more...
+          </p>
+        </div>
+      )}
+    </Fragment>
+  );
+}
+
+export default CardDetails;
